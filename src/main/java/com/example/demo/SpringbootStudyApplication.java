a12fe13e7f88f6085ba296f27caa7933e8315a6e
DESKTOP-F74MC34\hjlee <jacker97@naver.com>
2024-10-05 16:06:26 +0900

    프로젝트 디렉토리 정리

--- src/main/java/com/example/demo/SpringbootStudyApplication.java
+++ src/main/java/com/example/demo/SpringbootStudyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringbootStudyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SpringbootStudyApplication.class, args);
     }
 
 }
